ff533c9e081134ff161ea1b0c829ed6a97593c46
Michal MALOVECKY <mal20422@spengergasse.at>
2023-12-10 18:29:51 +0100

    mongo-imports

---
+++ LiquidAPI/LiquidGamesDatabase.cs
@@ -0,0 +1,51 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+public class LiquidGamesDatabase
+{
+    public MongoClient Client { get; }
+    public IMongoDatabase Db { get; }
+    public IMongoCollection<Genre> Genres => Db.GetCollection<Genre>("genres");
+
+    public LiquidGamesDatabase(string connectionString, string databaseName)
+    {
+        var settings = MongoClientSettings.FromConnectionString(connectionString);
+        Client = new MongoClient(settings);
+        Db = Client.GetDatabase(databaseName);
+    }
+
+    public class Genre
+    {
+        public ObjectId Id { get; set; }
+        public string GenreName { get; set; }
+        public List<Game> Games { get; set; } = new List<Game>();
+    }
+
+    public class Game
+    {
+        public int Rank { get; set; }
+        public string GameName { get; set; }
+        public string Platform { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Publisher { get; set; }
+        public double NA_Sales { get; set; }
+        public double EU_Sales { get; set; }
+        public double JP_Sales { get; set; }
+        public double Other_Sales { get; set; }
+        public double Global_Sales { get; set; }
+    }
+}
+
+public record CsvGameRecord
+{
+    public string Rank { get; set; }
+    public string Name { get; set; }
+    public string Platform { get; set; }
+    public string Year { get; set; }
+    public string Genre { get; set; }
+    public string Publisher { get; set; }
+    public string NA_Sales { get; set; }
+    public string EU_Sales { get; set; }
+    public string JP_Sales { get; set; }
+    public string Other_Sales { get; set; }
+    public string Global_Sales { get; set; }
+}
--- LiquidAPI/Program.cs
+++ LiquidAPI/Program.cs
@@ -1,25 +1,107 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+using MongoDB.Driver;
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    static int Main(string[] args)
+    {
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.Clear();
+
+        string connectionString = "mongodb://localhost:27017";
+        string databaseName = "liquidgames";
+
+        var liquidGamesDb = new LiquidGamesDatabase(connectionString, databaseName);
+
+        try
+        {
+            string csvFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "vgsales.csv");
+            SeedDatabase(liquidGamesDb, csvFilePath);
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine($"An error occurred: {ex.Message}");
+            return 1;
+        }
+
+        Console.WriteLine("Database seeding completed successfully.");
+        return 0;
+    }
+
+
+    static void SeedDatabase(LiquidGamesDatabase liquidGamesDb, string csvFilePath)
+    {
+        var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            HasHeaderRecord = true,
+        };
+
+        using var reader = new StreamReader(csvFilePath);
+        using var csv = new CsvReader(reader, config);
+
+        var records = csv.GetRecords<CsvGameRecord>().ToList();
+
+        foreach (var record in records)
+        {
+            // Handling potential 'N/A' values and parsing
+            if (!int.TryParse(record.Rank, NumberStyles.Any, CultureInfo.InvariantCulture, out int rank))
+            {
+                continue; // Skip records without a valid rank
+            }
+
+            if (!double.TryParse(record.NA_Sales, NumberStyles.Any, CultureInfo.InvariantCulture, out double naSales))
+            {
+                naSales = 0;
+            }
+
+            if (!double.TryParse(record.EU_Sales, NumberStyles.Any, CultureInfo.InvariantCulture, out double euSales))
+            {
+                euSales = 0;
+            }
+
+            if (!double.TryParse(record.JP_Sales, NumberStyles.Any, CultureInfo.InvariantCulture, out double jpSales))
+            {
+                jpSales = 0;
+            }
+
+            if (!double.TryParse(record.Other_Sales, NumberStyles.Any, CultureInfo.InvariantCulture,
+                    out double otherSales))
+            {
+                otherSales = 0;
+            }
+
+            if (!double.TryParse(record.Global_Sales, NumberStyles.Any, CultureInfo.InvariantCulture,
+                    out double globalSales))
+            {
+                globalSales = 0;
+            }
+
+            var game = new LiquidGamesDatabase.Game
+            {
+                Rank = rank,
+                GameName = record.Name,
+                Platform = record.Platform,
+                ReleaseYear = int.TryParse(record.Year, out int year) ? year : 0, // Handling year separately
+                Publisher = record.Publisher,
+                NA_Sales = naSales,
+                EU_Sales = euSales,
+                JP_Sales = jpSales,
+                Other_Sales = otherSales,
+                Global_Sales = globalSales,
+            };
+
+            // MongoDB filter and update definitions
+            var filter = Builders<LiquidGamesDatabase.Genre>.Filter.Eq(g => g.GenreName, record.Genre);
+            var update = Builders<LiquidGamesDatabase.Genre>.Update.Push(g => g.Games, game);
+
+            liquidGamesDb.Genres.FindOneAndUpdate(filter, update, new FindOneAndUpdateOptions<LiquidGamesDatabase.Genre>
+            {
+                IsUpsert = true
+            });
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
